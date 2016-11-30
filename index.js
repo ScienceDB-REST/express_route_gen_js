ab36abd981bbae60b012792708e66b765fdb58f7
Dr. Asis Hallab <asis.hallab@gmail.com>
2016-11-30 16:01:08 +0100

    Wrote first template to auto-generate routes (reading all models)

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 #!/usr/bin/env nodejs
 console.log( "Hello, world!" )
+
+var ejs = require( 'ejs' )
+var inflection = require( 'inflection' );
+
+// Parse command-line-arguments:
+var program = require('commander');
+program
+.arguments('<directory>')
+.option('--name <model_name>', 'The name of the model as provided to \'sequelize model:create\'.')
+.option('--attributes <model_attributes>', 'The model attributes as provided to \'sequelize model:create\'.')
+.action(function(directory) {
+  console.log('directory: %s name: %s attributes: %s',
+      directory, program.name, program.attributes);
+  ejs.renderFile( 'views/pages/controller_read.ejs', {
+    name: program.name,
+    namePl: inflection.pluralize( program.name ),
+    attributes: program.attributes
+  }, {}, function( err, str ) {
+   console.log(str) 
+  } )
+})
+.parse(process.argv);
